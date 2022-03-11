cbd6a82f35e74bf2df0d88adc5162ba928ccb18d
Lai Hock Chun <jasonlai29@gmail.com>
2022-03-11 11:07:59 +0800

    Feat changes

--- database/index.js
+++ database/index.js
@@ -4,7 +4,7 @@ const { db_user, db_password, db_name } = require("./config");
 const db = new Pool({
   user: db_user,
   password: db_password,
-  host: "localhost",
+  host: "ec2-34-242-89-204.eu-west-1.compute.amazonaws.com",
   database: db_name,
 });
 
